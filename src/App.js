1bd2b1b92240c12dafe5b6e7000659e659b50528
emi-fernandes <emillylafernandes@gmail.com>
2025-03-03 00:01:43 +0000

    commit 5

--- src/App.js
+++ src/App.js
@@ -6,10 +6,9 @@ function App() {
 
   const info = {
     personalfacts: "Hi! My name is Emilly Fernandes, I’m 21 years old, and I’m from Brazil. I have a deep love for animals, nature, and exploring new things. Being outdoors brings me balance, and I enjoy discovering new places, learning new skills, and experiencing challenges that push me to grow. I’m naturally curious and always on the move, seeking personal growth and fresh perspectives. I also have a tiny, beautiful dog named Mel, who fills my days with joy and companionship. She’s a big part of my life and makes every moment even more special. I believe that every experience shapes us, and I’m always ready for the next adventure!",
-    education: "I am currenly in the fourth semester of Data Science and AI at Ibmec.",
-    swimming: "I was a competitive swimmer for six years, learning teamwork and discipline.",
-    development: "I specialize in web development with React, HTML, CSS, and JavaScript.",
-    dataScience: "I am passionate about data science, machine learning, and AI applications."
+    education: "I’m currently in my fourth semester of Data Science and Artificial Intelligence at Ibmec, Brazil, and spending this semester at EM Normandie in Dublin. This exchange is giving me international academic experience and exposure to new perspectives. Before university, I studied at Rio International School, where I built a strong foundation in English and adaptability.",
+    swimming: "I have always been passionate about movement and physical activities. I practiced competitive swimming for six years, which taught me discipline, resilience, and teamwork. Before that, I trained in ballet from the age of 3 to 14, developing flexibility, balance, and dedication. More recently, I have been passionate about surfing, which allows me to connect with nature while constantly challenging myself to improve. ",
+    dataScience: "I worked as a web development intern for four months at Lance, a major sports media website in Brazil. During this time, I contributed to the development and maintenance of the platform, improving my skills in front-end development, problem-solving, and website optimization. This experience allowed me to work in a fast-paced environment, collaborate with a professional team, and gain hands-on knowledge in website development for a high-traffic sports platform."
   };
 
   return (
@@ -58,6 +57,17 @@ function App() {
           </div>
         )}
       </main>
+<section class="projects">
+  <h2>Projects</h2>
+  <div class="projects-grid">
+    <div class="project-item">Web Development<button>Click Here</button></div>
+    <div class="project-item">Front-end<button>Click Here</button></div>
+    <div class="project-item">Back-end<button>Click Here</button></div>
+    <div class="project-item">Agile Methods<button>Click Here</button></div>
+    <div class="project-item full-width">Graphic Design and User Experience<button>Click Here</button></div>
+  </div>
+</section>
+
       <footer id="contact" className="footer">
         <h2>Contato</h2>
         <a href="https://www.linkedin.com/in/emilly-fernandes-684155261" target="_blank" rel="noopener noreferrer" className="linkedin-link">
