76792eebe4c1a314dbd1b9a1e0a6bb4724d64e1d
emi-fernandes <emillylafernandes@gmail.com>
2025-03-12 17:54:40 +0000

    commit 18

---
+++ build/precache-manifest.30fb79ab6e3073a524cf02a65f470efa.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2911e1ddaafe331d21ea1bb94b2c5a4",
+    "url": "/emi-site/index.html"
+  },
+  {
+    "revision": "ee622c26573c0cb0325a",
+    "url": "/emi-site/static/css/main.d64ab062.chunk.css"
+  },
+  {
+    "revision": "c857a59abacde3546399",
+    "url": "/emi-site/static/js/2.b754d78d.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/emi-site/static/js/2.b754d78d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b77e45e308ff4c30cd4",
+    "url": "/emi-site/static/js/3.f8e92708.chunk.js"
+  },
+  {
+    "revision": "ee622c26573c0cb0325a",
+    "url": "/emi-site/static/js/main.9f6dae98.chunk.js"
+  },
+  {
+    "revision": "0ab6fb8b670ae424aac6",
+    "url": "/emi-site/static/js/runtime-main.20c8151d.js"
+  },
+  {
+    "revision": "e3b843d59b792fea851a4085ca05ed57",
+    "url": "/emi-site/static/media/emillyfotocapa.e3b843d5.JPG"
+  }
+]);
