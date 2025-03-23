6c6a97c8835f1cfed9a7fb30d024d476441aa3fb
emi-fernandes <emillylafernandes@gmail.com>
2025-03-23 02:22:25 +0000

    Atualiza modal e botão de fechar

--- src/App.js
+++ src/App.js
@@ -113,22 +113,27 @@ function App() {
           </div>
         </div>
       </section>
-
       {selectedInfo && (
-        <div className="modal-overlay" onClick={closeModal}>
-          <div className="modal" onClick={(e) => e.stopPropagation()}>
-            <p>{selectedInfo.text}</p>
-            {selectedInfo.images && selectedInfo.images.length > 0 && (
-              <div className="carousel-container">
-                <button className="arrow" onClick={handlePrev}>&lt;</button>
-                <img src={selectedInfo.images[currentImageIndex]} alt="Carousel" className="carousel-image" />
-                <button className="arrow" onClick={handleNext}>&gt;</button>
-              </div>
-            )}
-          </div>
-          <button className="close-button" onClick={closeModal}>X</button>
+  <div className="modal-overlay" onClick={closeModal}>
+    <div className="modal" onClick={(e) => e.stopPropagation()}>
+      <button className="close-button" onClick={closeModal}>X</button>
+      <p>{selectedInfo.text}</p>
+
+      {selectedInfo.images && selectedInfo.images.length > 0 && (
+        <div className="carousel-container">
+          <button className="arrow" onClick={handlePrev}>&lt;</button>
+          <img
+            src={selectedInfo.images[currentImageIndex]}
+            alt="Carousel"
+            className="carousel-image"
+          />
+          <button className="arrow" onClick={handleNext}>&gt;</button>
         </div>
       )}
+    </div>
+  </div>
+)}
+
            <section id="projects" className="projects-section">
    <h2>Projects</h2>
    <div className="projects-grid">
